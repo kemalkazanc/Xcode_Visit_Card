54e49cc52d281b1983e08ebb2dea68051f6a8b6d
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-08-16 18:38:04 +0200

    Initial Commit

---
+++ Visit Card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Visit Card
+//
+//  Created by Kemal Kazanc on 16/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visit Card/Visit_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visit_CardApp.swift
+//  Visit Card
+//
+//  Created by Kemal Kazanc on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Visit_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
